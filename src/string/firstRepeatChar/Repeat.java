bed6f71c628d89a0e912c5d155d2dacea1f8ba11
inoob <inoob@MSI>
2023-05-08 22:52:12 +0530

    08/05/2023

---
+++ src/string/firstRepeatChar/Repeat.java
@@ -0,0 +1,33 @@
+package string.firstRepeatChar;
+
+/*check 1st repeat char in String*/
+public class Repeat {
+	//char variable
+	final static  int MAX_CHAR=26;
+	//check repMethod
+	private static char rep(String s) {		
+		//hash array
+		int[] arr = new int[MAX_CHAR];
+		
+		//String update hash array
+		for(int i=0;i<s.length();i++) {
+			arr[s.charAt(i)-'a'] = ++arr[s.charAt(i)-'a'];
+			if(arr[s.charAt(i)-'a']>= 2) {
+				return s.charAt(i);
+			}
+		}
+		return ' ';
+	}
+	
+	
+	//main function
+	public static void main(String[] args) {
+		String s = "hellogeeks";
+		char ch = rep(s);
+		if(ch == ' ') {
+			System.out.println("-1");
+		}else {
+			System.out.println(ch);
+		}
+	}
+}
