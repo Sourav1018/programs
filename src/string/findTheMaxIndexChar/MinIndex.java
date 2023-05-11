c50e012b2048914721df535349a1ef000b2de292
inoob <inoob@192.168.244.1>
2023-05-12 00:14:42 +0530

    12/05/23

--- src/string/findTheMaxIndexChar/MinIndex.java
+++ src/string/findTheMaxIndexChar/MinIndex.java
@@ -2,45 +2,47 @@ package string.findTheMaxIndexChar;
 
 /* find the common char index from the 1st String*/
 public class MinIndex {
-	
-	//size of hash array
+
+	// size of hash array
 	private static final int size = 26;
-	//method of return index 
+
+	// method of return index
 	private static int index(String a, String b) {
-		
-		//hash array 
+
+		// hash array
 		int[] arr = new int[size];
-		
-		//update the String 1
-		for(int i=0;i<a.length();i++) {
+
+		// update the String 1
+		for (int i = 0; i < a.length(); i++) {
 			arr[a.charAt(i) - 'a'] = 1;
 		}
-		//update the String 2
-		for(int j=0;j<b.length();j++) {
+		// update the String 2
+		for (int j = 0; j < b.length(); j++) {
 			arr[b.charAt(j) - 'a'] = ++arr[b.charAt(j) - 'a'];
 		}
 		printArray(arr);
-		//finding index
-		for(int j=0;j<a.length();j++) {
-			if(arr[a.charAt(j) - 'a'] > 1) {
+		// finding index
+		for (int j = 0; j < a.length(); j++) {
+			if (arr[a.charAt(j) - 'a'] > 1) {
 				return j;
 			}
-		}		
+		}
 		return -1;
 	}
+
 	public static void main(String[] args) {
-		
+
 		String a = "adcffaet";
 		String b = "onkl";
-		
-		int index = index(a,b);
+
+		int index = index(a, b);
 		System.out.println(index);
 
 	}
-	
+
 	private static void printArray(int[] arr) {
-		for(int i=0;i<arr.length;i++) {
-			System.out.print(arr[i]+" ");
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
 		}
 		System.out.println();
 		return;
---
+++ src/string/lexicographicallyOrderCheck/Lexo.java
@@ -0,0 +1,55 @@
+package string.lexicographicallyOrderCheck;
+
+import java.util.*;
+public class Lexo {
+	
+	//check method
+	private static int check(String a, String b) {
+		
+		//convert into char array
+		char[] arr1 = a.toCharArray();
+		char[] arr2 = b.toCharArray();
+		
+		//empty string 
+		String str1 = "";
+		String str2 = "";
+		//convert into small 1st string
+		for(int i=0;i<arr1.length;i++) {
+			if(arr1[i] < 'a') {
+				str1+=(char)(arr1[i]+32);	
+			}else {
+			str1+=(char)(arr1[i]);
+			}
+		}
+		//convert into small 2nd string
+		for(int i=0;i<arr2.length;i++) {
+			if(arr2[i] < 'a') {
+				str2+=(char)(arr2[i]+32);
+				continue;
+			}
+			str2+=(char)(arr2[i]);
+		}
+		//compare string 1 and string 2
+		int i=0,j=0;
+		while(i<str1.length() && j<str2.length()) {
+			if(str1.charAt(i) < str2.charAt(j)) {
+				return -1;
+			}
+			else if(str1.charAt(i) > str2.charAt(j)) {
+				return 1;
+			}
+			i++;j++;
+		}
+		return 0;
+	}
+	//main method
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String a = sc.next();
+		String b = sc.next();
+		int i=check(a,b);
+		System.out.println(i);	
+		sc.close();
+	}
+
+}
